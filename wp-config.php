48fbff7902ab994351d61df79236ce9bde082e93
root <root@ip-172-31-19-236.ec2.internal>
2015-02-25 08:22:31 +0000

    Add .htaccss file

--- wp-config.php
+++ wp-config.php
@@ -3,16 +3,16 @@
 
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('ecap_db_name'));
+define('DB_NAME', 'ecap_wp_db');
 
 /** MySQL database username */
-define('DB_USER', getenv('ecap_db_user'));
+define('DB_USER', 'egtlabsecap');
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('ecap_db_password'));
+define('DB_PASSWORD', '6cc7c5a5a21978');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('ecap_db_host'));
+define('DB_HOST', 'egtlabsecap.ciq9ul9lvuj5.us-east-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -20,14 +20,14 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('AUTH_KEY',         ',AXaI&Ufl4,O]SA3|@;zbhu=1#{WMO3]oDEbwZMsn4hmL&k6Onr&UdaSm4gH_vj1');
-define('SECURE_AUTH_KEY',  '6$5P~r~I$u2vTv-cGwV|PcMt[YX*yjryTp$Al-,l|)p% JF/Z_m|d2(3C}Fexo`d');
-define('LOGGED_IN_KEY',    'zz1drbs0x~|%8 j*%1X1pRs8P??c?|I8fi}uq2L3d|)}Y}12W R|`%$w8Zi-`4N6');
-define('NONCE_KEY',        'a-3;t`CBg:-: 2zZXt(es/QX9gc7I?V^3=3Mu(DbP*Z|m5j)5VSlgxEa>/wqFFI{');
-define('AUTH_SALT',        '3i]RP>}e`SDA&ia7*Z+m}VwU2n,|g!_Ysot*0Up_4<>;*&i/:x.!Re4|?QP:JXc4');
-define('SECURE_AUTH_SALT', '7MwDA~Y$Xizxz8dW-Z:o$QY^4bq4ug0v+OUcv4;]%D9Utz:pxoIMC<bLUs|Wp[rs');
-define('LOGGED_IN_SALT',   '4,-0![%KEhw*Ak-SJ7F_[JBKXn+@Ld,HpCN7f|YLT4oS=^x~Yr;w;(Di-4&tU%T?');
-define('NONCE_SALT',       '~_d+U*2wJb6iExBu)vGlm<U@sm6=U7|P`TOOrf3q_$B0%b}dDjra1M*:1WKITd~1');
+define('AUTH_KEY',         '~U-M{Y+-U6+@BU)|%P_<z~X-dV~q<Ji~BP)p)Mnu7I}8Yhji, <;)<-qHi$%R?-D');
+define('SECURE_AUTH_KEY',  'KJW>=VXQ&<Ubt4IyZd/L*@ R$N6pDL)d-7h!44|MbpG|+~^KO+Vm/thXmm;Z.2#V');
+define('LOGGED_IN_KEY',    'zZgXdj7J]l(ccoy.IAJB!D6g4SyD^)nJ|5IJ3tBPC1&G49M6`F@+aTW1*4E@gHn1');
+define('NONCE_KEY',        '^,OP|W[pX~k`w&k(FFCqSsg{zFJmAk,ut[0Jy5^Ja450dwKZk>}l0psh5-kR:S+4');
+define('AUTH_SALT',        '=:n_.~*NIP5y71K;-4<&U!HNCXv`^Zr $,No-G(Wv#id*QQqhU_-h=f!;eJl>02r');
+define('SECURE_AUTH_SALT', '-b|uVlR#VWrS?zC9iFO@!:}{)+e@UBRTe!{~l-;%E/L=#  k s5(k=ru Du(zT>|');
+define('LOGGED_IN_SALT',   'Wg7^BVvZ5y=Y+iD/<)jVB;np<b5Qnux<R^aty.Aw9FWU|W5x8#*lygC+sh||o*Y5');
+define('NONCE_SALT',       'OOdtJ-BV_y+F-i`6Xbp4F+pO+;?<1J`=@De+f>.FV;kh1,$.WsiBzXWHW3`N?r2y');
 
 
 $table_prefix = 'wp_';
@@ -39,7 +39,7 @@ $table_prefix = 'wp_';
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 $base = '/';
-define('DOMAIN_CURRENT_SITE', getenv('ecap_domain'));
+define('DOMAIN_CURRENT_SITE', 'cap.egt-labs.com');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
